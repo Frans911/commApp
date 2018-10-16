b929f07a8c762c7206ad1ce675b0826e43708cf3
Mahlangu1993 <212281972@tut4life.ac.za>
2018-10-16 15:24:37 +0200

    Google plus button fixed

--- src/pages/viewjobs/viewjobs.module.ts
+++ src/pages/viewjobs/viewjobs.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ViewjobsPage } from './viewjobs';
-import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
   declarations: [
     ViewjobsPage,
   ],
   imports: [
-    IonicImageLoader,
     IonicPageModule.forChild(ViewjobsPage),
   ],
 })
